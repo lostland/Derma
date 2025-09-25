4b6e779cafb67dd9b2352313f64cf931ab1ea2a0
lostland <kimcs71@gmail.com>
2025-09-25 05:29:52 +0000

    ...

--- client/src/pages/admin.tsx
+++ client/src/pages/admin.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-
+import React from "react";
 import { useTheme } from "@/context/ThemeContext";
 
 export default function AdminPage() {
